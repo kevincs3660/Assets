4a9b36169e7e04e63ffa6522d299673b33919f28
kevincs3660 <u0822559@utah.edu>
2015-09-19 16:51:38 -0600

    Added movement and basic attack scripts

---
+++ Scripts/PlayerController.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+using Prime31;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed = 8;
+	public float gravity = -35;
+	public float jumpHeight = 5;
+
+	private CharacterController2D _controller;
+	private WeaponScript[] _weapons;
+	private bool facingRight = true;
+	// Use this for initialization
+
+	void Awake()
+	{
+		_weapons = GetComponentsInChildren<WeaponScript>();
+	}
+
+	void Start () {
+		_controller = gameObject.GetComponent<CharacterController2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Vector3 velocity = _controller.velocity;
+		float inputX = Input.GetAxis ("Horizontal");
+		Vector2 shotDirection = Vector2.zero;
+
+		//Debug.Log ("Axis is: " + inputX.ToString ());
+
+		velocity.x = 0;
+
+		if ( inputX < 0) 
+		{
+			velocity.x = -speed;
+			facingRight = false;
+			//shotDirection = -transform.right;
+		}
+		else if (inputX > 0)
+		{
+			velocity.x = speed;
+			facingRight = true;
+			//shotDirection = transform.right;
+		}
+
+		if (Input.GetAxis ("Jump") > 0 && _controller.isGrounded) {
+			velocity.y = Mathf.Sqrt (2f * jumpHeight * -gravity);
+		}
+
+		bool shoot = Input.GetButtonDown ("Fire1");
+		shoot |= Input.GetButtonDown ("Fire2");
+
+		if (shoot) 
+		{
+			if(_weapons[0] != null && _weapons[1] != null)
+				if(inputX > 0)
+					_weapons[0].Attack(false);
+				else if(inputX < 0)
+					_weapons[1].Attack(false);
+				else
+				{
+					if(facingRight)
+					_weapons[0].Attack(false);
+					else
+					_weapons[1].Attack(false);
+				}
+			
+				//_weapons[0].Attack(false, shotDirection);
+		}
+
+		velocity.y += gravity * Time.deltaTime;
+
+		_controller.move (velocity * Time.deltaTime);
+	}
+}
